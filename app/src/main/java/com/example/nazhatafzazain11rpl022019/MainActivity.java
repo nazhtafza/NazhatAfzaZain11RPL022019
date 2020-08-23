ed0f5c391985554a892ad2e1b32108b97be36203
nazhatafz <nazhat.10rpl2.2019@gmail.com>
2020-08-23 14:40:04 +0700

    tambah textview menuju ke registrasi

--- app/src/main/java/com/example/nazhatafzazain11rpl022019/MainActivity.java
+++ app/src/main/java/com/example/nazhatafzazain11rpl022019/MainActivity.java
@@ -8,39 +8,49 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
- SharedPreferences pref;
- SharedPreferences.Editor editor;
-EditText txtusername;
-EditText txtpassword;
-Button btnlogin;
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    EditText txtusername;
+    EditText txtpassword;
+    Button btnlogin;
+    TextView tvdaftar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        pref = getSharedPreferences("login",MODE_PRIVATE);
-        txtusername =(EditText)findViewById(R.id.txtusername);
-        txtpassword = (EditText)findViewById(R.id.txtpassword);
-        btnlogin = (Button)findViewById(R.id.btnlogin);
+        pref = getSharedPreferences("login", MODE_PRIVATE);
+        txtusername = (EditText) findViewById(R.id.txtusername);
+        txtpassword = (EditText) findViewById(R.id.txtpassword);
+        tvdaftar = (TextView) findViewById(R.id.tvdaftar);
+        btnlogin = (Button) findViewById(R.id.btnlogin);
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (txtusername.getText().toString().equalsIgnoreCase("minke")
-                    && txtpassword.getText().toString().equalsIgnoreCase("gamteng")){
+                        && txtpassword.getText().toString().equalsIgnoreCase("gamteng")) {
 //save to sharedpreferences
                     editor = pref.edit();
-                    editor.putString("username",txtusername.getText().toString());
-                    editor.putString("status","login");
+                    editor.putString("username", txtusername.getText().toString());
+                    editor.putString("status", "login");
                     editor.apply();
                     //menuju main menu
                     startActivity(new Intent(getApplicationContext(), Mainmenu.class));
                     finish();
 
                 }
+
             }
         });
-
-
+        tvdaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Registrasi.class));
+            }
+        });
     }
 }
+
---
+++ app/src/main/java/com/example/nazhatafzazain11rpl022019/Registrasi.java
@@ -0,0 +1,24 @@
+package com.example.nazhatafzazain11rpl022019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Registrasi extends AppCompatActivity {
+TextView tvlogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrasi);
+        tvlogin = (TextView)findViewById(R.id.tvlogin);
+        tvlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Registrasi.this,MainActivity.class));
+            }
+        });
+    }
+}
